7f9b2e85de07600ff3a34d7bb65a8196d6d4f7ec
“runyu <r.ma-8@student.tudelft.nl>
2024-01-04 19:41:36 +0100

    Add short

---
+++ sbatch/short/gen.py
@@ -0,0 +1,39 @@
+# Define your seeds and eps
+seeds = [0, 1, 2, 3, 4, 5, 6]  # Example seed values
+eps = [0.0, 0.05, 0.1, 0.15,0.2]  # Example ep values
+
+template = """#!/bin/sh
+
+#SBATCH --partition=general
+#SBATCH --qos=long
+#SBATCH --ntasks=1
+#SBATCH --time=60:00:00
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=16000
+#SBATCH --mail-type=END
+
+module use /opt/insy/modulefiles
+module load miniconda/3.9
+# module load cuda/11.2 cudnn/11.2-8.1.1.33
+
+conda activate ra
+
+cd /tudelft.net/staff-umbrella/rarma/src/RA/
+python train/train.py--render f --save t --seed {seed} --iters 80000 --save_freq 2500 --device 1 --ep {ep}
+"""
+
+# Create files
+for seed in seeds:
+    for ep in eps:
+        # Create a filename based on the seed and ep
+        filename = f"sbatch/short/train_{seed}_{ep}.sh"
+        
+        # Replace placeholders in the template
+        file_content = template.format(seed=seed, ep=ep)
+        
+        # Write to a file
+        with open(filename, 'w') as file:
+            file.write(file_content)
+            print(f"Created file: {filename}")
+
+print("All files created.")
---
+++ sbatch/short/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Define your seeds and eps
+seeds=(0 1 2 3 4 5 6)  # Example seed values
+eps=(0.0 0.1 0.05 0.15 0.2)  # Example ep values
+
+# Loop through seeds and eps and run sbatch for each file
+for seed in "${seeds[@]}"; do
+    for ep in "${eps[@]}"; do
+        file_name="sbatch/short/train_${seed}_${ep}.sh"
+        sbatch "$file_name"
+        echo "Submitted $file_name"
+    done
+done
+
+echo "All scripts submitted to sbatch."
